91278c095c86029455ea9e132677090290f30ce4
thedoodler <ramprakashsmail@gmail.com>
2020-12-19 21:30:55 +0530

    START:  explosion animation

--- appealing_randomness/static/anims.js
+++ appealing_randomness/static/anims.js
@@ -1,54 +1,3 @@
-function rotate_anim(x,y,i){
-  this.pos = createVector(x,y);
-  // this.id = i;
-  this.id = map(i, 0, 5, 3, 1);
-  this.ang = i;
-  this.size = 5;
-  this.rad = 100;
-  this.lifetime = 200;
-}
-
-// rotate_anim.prototype.update = function(){
-//   this.ang += 0.025 * (this.id + 1);
-//   this.lifetime--;
-// }
-
-rotate_anim.prototype.update = function(){
-  // this.rad += 1;
-  this.ang += 0.025*this.id;
-  this.lifetime--;
-}
-
-rotate_anim.prototype.show = function(){
-  circle(this.pos.x + cos(this.ang) * this.rad, this.pos.y + sin(this.ang) * this.rad, this.size);
-}
-
-
-function rotor_animation(x,y,i,dir){
-  this.alive = true;
-  this.pos = createVector(x,y);
-  this.show_pos = this.pos.copy();
-  this.id = map(i, 0, 5, 3, 1);;
-  this.dir = dir;
-  this.ang = map(i, 0, 5, 0, 360);
-  this.size = 5;
-  this.rad = 100;
-  this.lifetime = 200;
-}
-
-rotor_animation.prototype = {
-  update: function(){
-    // this.ang += 0.3*(5-this.id+1);
-    this.ang += 3*(this.id);
-    angleMode(DEGREES);
-    this.show_pos.y = this.pos.y + map(sin(this.ang),-1,1,-this.rad/2,this.rad/2);
-    this.show_pos.x = this.pos.x + map(cos(this.ang),-1,1,-this.rad/2,this.rad/2);
-  },
-
-  show: function(){
-    circle(this.show_pos.x,this.show_pos.y,this.size);
-  }
-}
 
 function power_animation(x,y,i,dir,rad){
   this.alive = true;
@@ -58,10 +7,10 @@ function power_animation(x,y,i,dir,rad){
   this.dir = dir;
   // this.val = random(0.4,1.4);
   this.ang = map(i, 0, 5, 0, 360);
-  this.size = 2;
+  this.size = 2.5;
   this.rad = rad;
   this.lifetime = power_lifetime;
-  this.alpha = random(60,100);
+  this.alpha = random(60,150);
 }
 
 power_animation.prototype = {
@@ -99,3 +48,13 @@ power_animation.prototype = {
 
   }
 }
+
+
+function explosion_animation(x,y){
+  this.alive = true;
+  this.pos = createVector(x,y);
+  this.size = random(2,7);
+  this.speed = createVector(random(3,10),random(3,10));
+}
+
+function
